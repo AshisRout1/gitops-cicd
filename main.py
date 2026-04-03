a634010e1ac633ec7db0485a955f1412e70f21f9
AshisRout1 <157779249+AshisRout1@users.noreply.github.com>
2026-04-04 02:35:51 +0530

    Add health check endpoint and secure environment variable
    Updated the Flask app to include a health check endpoint and improved environment variable usage for security.

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-from flask import Flask, request
+from flask import Flask
 import os
+
 
 def create_app():
     app = Flask(__name__)
 
-    
     @app.get("/")
     def index():
-        message = os.getenv("MESSAGE", "Hello from ArgoCD with Github Actions on Kubernetes! 🚀")
-        # Return HTML with green message
+        # Safe environment variable usage (no secrets hardcoded)
+        message = os.getenv(
+            "MESSAGE",
+            "Hello from Argocd running on Kubernetes 🚀"
+        )
+
         return f"""
         <html>
-            <head><title>Python Docker App</title></head>
+            <head><title>Secure Python App</title></head>
             <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
                 <h1 style="color: black;">{message}</h1>
             </body>
         </html>
         """
 
+    @app.get("/health")
+    def health():
+        return {"status": "ok"}, 200
+
     return app
+
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8000")), debug=True)
+    # No debug mode (important for security scan)
+    port = int(os.getenv("PORT", "8000"))
+    app.run(host="0.0.0.0", port=port)

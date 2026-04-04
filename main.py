dcf63a7e24bc9e2eba5b85e7c19291ed23d0c212
AshisRout1 <157779249+AshisRout1@users.noreply.github.com>
2026-04-04 20:46:48 +0530

    Refactor Flask app structure and routes

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 from flask import Flask
 import os
 
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
+@app.route("/")
+def index():
+    return os.getenv("MESSAGE", "Hello from ArgoCD Kubernetes 🚀")
 
-    @app.get("/")
-    def index():
-        message = os.getenv(
-            "MESSAGE",
-            "Hello from Argocd running on Kubernetes 🚀"
-        )
-
-        return f"""
-        <html>
-            <head><title>Secure Python App</title></head>
-            <body style="font-family: Arial; text-align:center; margin-top:50px;">
-                <h1>{message}</h1>
-            </body>
-        </html>
-        """
-
-    @app.get("/health")
-    def health():
-        return {"status": "ok"}, 200
-
-    return app
-
-
-# 👇 IMPORTANT: expose app for Gunicorn
-app = create_app()
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
